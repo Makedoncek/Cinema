b62bd3d966b318b5e29db6f967c4b11726996541
Oleksii Savenko <123456v64@gmail.com>
2022-12-04 21:49:27 +0200

    init commit

---
+++ scripts/app.js
@@ -0,0 +1,71 @@
+function createSorting() {
+  const sort = new URLSearchParams(location.search).get('sort');
+
+  const nav = `
+    <a class="sort__item ${sort === 'name' ? 'active' : ''}" href="${location.origin + location.pathname + '?sort=name'}">Назва</a>
+    <a class="sort__item ${sort === 'date' ? 'active' : ''}" href="${location.origin + location.pathname + '?sort=date'}">Дата</a>
+  `;
+
+  return createFragmentTemplate(nav);
+}
+
+function createContentTemplate(movie) {
+  const article = `<article class="card">
+    <header class="card__header" style="background-image: url(${movie.image})">
+      <h2 class="card__title">${movie.title}</h2>
+      <span class="card__info">${new Date(movie.date).getFullYear()} - ${movie.duration}</span>
+    </header>
+    <section class="card__content">
+      <p class="card__description">${movie.description}</p>
+      <hr>
+      <p>Режисер: ${movie.director}</p>
+    </section>
+  </article>`;
+
+  return createFragmentTemplate(article);
+}
+
+function createFragmentTemplate(str) {
+  const template = document.createElement('template');
+
+  template.innerHTML = str;
+
+  return template.content;
+}
+
+function appendContent(id, content) {
+  const el = document.getElementById(id);
+
+  el.appendChild(content);
+}
+
+function sortMovies(data) {
+  const sort = new URLSearchParams(location.search).get('sort');
+
+  switch(sort) {
+    case 'name':
+      return data.sort((a, b) => a.title.localeCompare(b.title));
+    case 'date':
+      return data.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+    default:
+      return data;
+  }
+}
+
+function init() {
+  fetch('https://run.mocky.io/v3/1be5a8b1-2625-488e-95a9-83a508b6cb34')
+    .then((res) => res.json())
+    .then(sortMovies)
+    .then((data) => {
+      const fragment = document.createDocumentFragment();
+
+      data.forEach((movie) => {
+        fragment.appendChild(createContentTemplate(movie));
+      });
+
+      appendContent('content', fragment);
+      appendContent('sort', createSorting());
+    });
+}
+
+init();
